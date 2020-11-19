a103860918f78d6227d209c0ed79cab959a74c4e
davidjhoseph <davidjoe781227@gmail.com>
2020-11-19 10:44:18 +0100

    Intitiaql Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="main">
+        
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+// import {gsap} from 'gsap';
+// import ScrollTrigger from 'gsap/ScrollTrigger';
 export default {
   name: 'home',
-  components: {
-    HelloWorld
+  mounted(){
+      this.firstAnimations()
+  },
+  components:{
+  },
+  methods: {
+    //   firstAnimations(){
+    //       var tl = gsap.timeline();
+    //       tl.from(this.$refs.first, {y:2000, autoAlpha: 0, duration:1});
+    //       tl.from(this.$refs.second, {y:-2000, autoAlpha: 0, duration:1},)
+    //       tl.from(this.$refs.third, {y:2000, autoAlpha: 0, duration:1},)
+    //       tl.from(this.$refs.circleSmall, {x:-500, duration:1},);
+    //       tl.from(this.$refs.circleLarge, {x:1000, duration:1},"-=1");
+    //       tl.from(this.$refs.watch, {x:1000, y:500, duration:1,},"-=0.5");
+    //       tl.from(this.$refs.heading, {x:-1000, duration:1,}, "-=1");
+    //       tl.from(this.$refs.lifestyle, {width:0, duration:1},"-=0.5");
+    //   }
   }
 }
 </script>
+<style lang="scss" scoped>
+
+
+
+
+</style>
---
+++ tailwind.js
@@ -0,0 +1,21 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [
+  ],
+  theme: {
+    extend: {
+        colors:{
+            'brand-dark': '#2b2b2b',
+            'brand-purple': '#7d1eff'
+        },
+        fontFamily: {
+            'poppins': ['Poppins']
+        }
+    },
+  },
+  variants: {},
+  plugins: [],
+};
